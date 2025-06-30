b15e78f9c9dfbcfd9a9814c1df353e408c793642
DamyanBG <d.dimitrov.1988@abv.bg>
2025-06-30 14:09:42 +0300

    change url vaiues

--- src/api/productsApi.ts
+++ src/api/productsApi.ts
@@ -2,41 +2,41 @@ import type { Product, ProductData } from "@/lib/types";
 import { PRODUCTS_URL } from "@/lib/urls";
 
 export const getProducts = async (): Promise<Product[]> => {
-    const resp = await fetch(PRODUCTS_URL)
-    const data: Product[] = await resp.json()
-    return data
-}
+  const resp = await fetch(PRODUCTS_URL);
+  const data: Product[] = await resp.json();
+  return data;
+};
 
 export const postProduct = async (productData: ProductData): Promise<Product> => {
-    const resp = await fetch(PRODUCTS_URL, {
-        method: "POST",
-        body: JSON.stringify(productData),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    const data: Product = await resp.json()
-    return data
-}
+  const resp = await fetch(PRODUCTS_URL, {
+    method: "POST",
+    body: JSON.stringify(productData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: Product = await resp.json();
+  return data;
+};
 
 export const deleteProduct = async (id: string) => {
-    const url = `${PRODUCTS_URL}${id}`;
-    const resp = await fetch(url, {
-        method: "DELETE",
-    });
-    if (!resp.ok) {
-        throw Error();
-    }
+  const url = `${PRODUCTS_URL}${id}`;
+  const resp = await fetch(url, {
+    method: "DELETE",
+  });
+  if (!resp.ok) {
+    throw Error();
+  }
 };
 
 export const editProduct = async (product: Product): Promise<Product> => {
-    const resp = await fetch(PRODUCTS_URL, {
-        method: "PUT",
-        body: JSON.stringify(product),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    const data: Product = await resp.json()
-    return data
-}
+  const resp = await fetch(PRODUCTS_URL, {
+    method: "PUT",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: Product = await resp.json();
+  return data;
+};
--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,7 +1,7 @@
-const API_URL = "http://127.0.0.1:8001";
+// const API_URL = "http://127.0.0.1:8001";
 // const ADMIN_AGENT_URL = "http://127.0.0.1:8002"
 
-// const API_URL = "https://be-memcommerce-667915768397.europe-west1.run.app";
+const API_URL = "https://be-memcommerce-667915768397.europe-west1.run.app";
 const ADMIN_AGENT_URL = "https://admin-agent-667915768397.europe-west1.run.app"
 
 export const CATEGORIES_URL = `${API_URL}/categories/`;
