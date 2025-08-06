16ff5c671a98073b3905f306899b398c02ae78c4
tihomirtx88 <tihomirtx88@gmail.com>
2025-08-06 12:04:25 +0300

    Edit size action
    Implement edit size action

--- src/api/sizesApi.ts
+++ src/api/sizesApi.ts
@@ -2,27 +2,45 @@ import type { Size, SizeData } from "@/lib/types";
 import { SIZES_URL } from "@/lib/urls";
 
 export const getSizes = async (): Promise<Size[]> => {
-    const resp = await fetch(SIZES_URL);
-    const data: Size[] = await resp.json();
-    return data;
+  const resp = await fetch(SIZES_URL);
+  const data: Size[] = await resp.json();
+  return data;
 };
 
 export const postSize = async (sizeData: SizeData): Promise<Size> => {
-    const resp = await fetch(SIZES_URL, {
-        method: "POST",
-        body: JSON.stringify(sizeData),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    const data: Size = await resp.json();
-    return data;
+  const resp = await fetch(SIZES_URL, {
+    method: "POST",
+    body: JSON.stringify(sizeData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: Size = await resp.json();
+  return data;
 };
 
 export const deleteSize = async (id: string) => {
-    const url = `${SIZES_URL}${id}`;
-    const resp = await fetch(url, {
-        method: "DELETE",
-    });
-    if (!resp.ok) throw Error();
+  const url = `${SIZES_URL}${id}`;
+  const resp = await fetch(url, {
+    method: "DELETE",
+  });
+  if (!resp.ok) throw Error();
+};
+
+export const editSize = async (size: Size): Promise<Size> => {
+  const url = `${SIZES_URL}${size.id}`;
+  const resp = await fetch(url, {
+    method: "PUT", 
+    body: JSON.stringify(size),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!resp.ok) {
+    throw new Error("Failed to update size");
+  }
+
+  const data: Size = await resp.json();
+  return data;
 };
