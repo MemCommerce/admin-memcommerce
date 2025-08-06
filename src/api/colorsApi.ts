6149b612d82033ae15f35fa41d46e95b3417714f
tihomirtx88 <tihomirtx88@gmail.com>
2025-08-06 12:55:15 +0300

    Implement edit color action
    Implement edit color action

--- src/api/colorsApi.ts
+++ src/api/colorsApi.ts
@@ -2,29 +2,47 @@ import type { ColorData, Color } from "@/lib/types";
 import { COLORS_URL } from "@/lib/urls";
 
 export const postColor = async (colorData: ColorData): Promise<Color> => {
-    const resp = await fetch(COLORS_URL, {
-        method: "POST",
-        body: JSON.stringify(colorData),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    const data: Color = await resp.json();
-    return data;
+  const resp = await fetch(COLORS_URL, {
+    method: "POST",
+    body: JSON.stringify(colorData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: Color = await resp.json();
+  return data;
 };
 
 export const getColors = async (): Promise<Color[]> => {
-    const resp = await fetch(COLORS_URL);
-    const data: Color[] = await resp.json();
-    return data;
+  const resp = await fetch(COLORS_URL);
+  const data: Color[] = await resp.json();
+  return data;
 };
 
 export const deleteColor = async (id: string) => {
-    const url = `${COLORS_URL}${id}`;
-    const resp = await fetch(url, {
-        method: "DELETE",
-    });
-    if (!resp.ok) {
-        throw Error();
-    }
+  const url = `${COLORS_URL}${id}`;
+  const resp = await fetch(url, {
+    method: "DELETE",
+  });
+  if (!resp.ok) {
+    throw Error();
+  }
+};
+
+export const editSize = async (size: Color): Promise<Color> => {
+  const url = `${COLORS_URL}${size.id}`;
+  const resp = await fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(size),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!resp.ok) {
+    throw new Error("Failed to update color");
+  }
+
+  const data: Color = await resp.json();
+  return data;
 };
